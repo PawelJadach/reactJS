ed646a549f8dd2f63284bff60613fec97261b184
Paweł Jadach <pawel.jadach.96@gmail.com>
2019-12-10 18:10:18 +0100

    add lists

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Home.scss';
-import List from '../List/ListContaner';
-import Search from '../Search/SearchContainer';
+import ListLink from '../ListLink/ListLink';
+// import Search from '../Search/SearchContainer';
 import PropTypes from 'prop-types';
 // import Container from '../Container/Container';
 
@@ -18,9 +18,8 @@ class Home extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
         {lists.map(listData => (
-          <List key={listData.id} {...listData} />
+          <ListLink key={listData.id} {...listData} />
         ))}
       </main>
     );
